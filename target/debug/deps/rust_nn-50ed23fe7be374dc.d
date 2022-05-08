56cb299cd381e803e59379fc9f94ed0588dbff5c
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-05-08 20:22:34 +0000

    Tweak Cargo.toml

---
+++ target/debug/deps/rust_nn-50ed23fe7be374dc.d
@@ -0,0 +1,5 @@
+/home/runner/rustnn/target/debug/deps/rust_nn-50ed23fe7be374dc: src/main.rs
+
+/home/runner/rustnn/target/debug/deps/rust_nn-50ed23fe7be374dc.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_nn.d
@@ -0,0 +1,1 @@
+/home/runner/rustnn/target/debug/rust_nn: /home/runner/rustnn/src/main.rs
