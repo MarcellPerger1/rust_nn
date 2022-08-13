609c1523a052ca8c4699c22635b95128779fd78d
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-08-13 13:04:32 +0000

    Fix error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::cell::RefCell;
-
 pub mod sigmoid;
+
+use std::cell::RefCell;
+use crate::sigmoid::Sigmoid;
 
 fn error_f(a: f64, b: f64) -> f64 {
     (a - b).powi(2) // cleaner and (with optimisations) probably just as fast as *
