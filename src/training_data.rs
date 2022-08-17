c22b4b3b145c11ffe71b9d79c8fc018b96a197e9
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-08-17 19:48:01 +0000

    Add `TrainingData` (newtype pattern)

--- src/training_data.rs
+++ src/training_data.rs
@@ -9,3 +9,13 @@ impl TrainingExample {
         TrainingExample {inputs: inputs.clone(), expected: expected.clone()}
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct TrainingData (Vec<TrainingExample>);
+
+impl TrainingData {
+    pub fn chunks(&self, size: usize) -> Vec<TrainingData>{
+        self.0.chunks(size).map(|c| TrainingData(c.to_vec())).collect()
+    }
+}
+
